b9e58ebd728793516a21f0610c524580f85bb6a4
Alessandro Passarelli <passarelli598@gmail.com>
2023-04-29 20:38:14 +0200

    Added some Pages with Routes

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/albums', function() {
-    return Album::all();
-});
-
-
 Route::get('/users', function() {
    return User::with('albums')
        ->paginate(5);
@@ -31,3 +22,5 @@ Route::get('/users', function() {
 Route::get('/photos', function() {
    return Photo::all();
 });
+
+Route::get('/', [AppController::class, 'staff']);
